397eca8c698500f36a9698fc7d5ba9cc1ec79841
Fernando Lopez <flopez00032@gmail.com>
2022-11-21 15:12:02 -0600

    created show all and form html

--- src/main/java/com/example/springpractice/controllers/MovieController.java
+++ src/main/java/com/example/springpractice/controllers/MovieController.java
@@ -2,7 +2,23 @@ package com.example.springpractice.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/movies")
 public class MovieController {
+
+    @GetMapping("showAll")
+    public String showMovies(){
+        return "movies/showAll";
+    }
+
+    @GetMapping("addMovie")
+    public String addMovieForm(){
+        return "movies/addMovie";
+    }
+
+
 }
