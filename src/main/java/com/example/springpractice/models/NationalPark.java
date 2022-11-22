869e192f7ec152cc31d8fe8406984430eb5233a2
Fernando Lopez <flopez00032@gmail.com>
2022-11-22 10:32:02 -0600

    national park model;

---
+++ src/main/java/com/example/springpractice/models/NationalPark.java
@@ -0,0 +1,42 @@
+package com.example.springpractice.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class NationalPark {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+
+//    constructor
+
+    public NationalPark() {
+    }
+
+
+
+//    setters and getters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
